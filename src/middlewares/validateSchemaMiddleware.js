282702c33a87e5f122270a20a90b593d00f7dae7
neves-gabriel <gabrielrn.mobres@gmail.com>
2022-03-18 03:08:11 -0300

    fix: :ambulance:

--- src/middlewares/validateSchemaMiddleware.js
+++ src/middlewares/validateSchemaMiddleware.js
@@ -2,6 +2,7 @@ export function validateSchemaMiddleware(schema) {
   return (req, res, next) => {
     const validation = schema.validate(req.body);
     if (validation.error) {
+      console.error(error);
       return res.sendStatus(400);
     }
 
