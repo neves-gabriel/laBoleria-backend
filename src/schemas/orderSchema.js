5eac732be38a10b0b7d3dd5d6a0b7cf4f5bb2fad
neves-gabriel <gabrielrn.mobres@gmail.com>
2022-03-20 18:06:00 -0300

    fix: :ambulance: another joi typo

--- src/schemas/orderSchema.js
+++ src/schemas/orderSchema.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 const orderSchema = Joi.object({
   clientId: Joi.string().required(),
   cakeId: Joi.string().required(),
-  quantity: Joi.number().greater(0).lesser(5).required(),
+  quantity: Joi.number().greater(0).less(5).required(),
 });
 
 export default orderSchema;
